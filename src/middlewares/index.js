b1163b0ae4ce946a5ca9fc4c7b3ecca73153b1ee
ajayraj2001 <ajayraj072001@gmail.com>
2025-03-16 23:01:36 +0530

    hey

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,9 +2,11 @@ const authenticateAdmin = require('./authenticateAdmin');
 const authenticateUser = require('./authenticateUser');
 const {getFileUploader} = require('./fileUpload');
 const {getMultipleFilesUploader} = require('./multipleFileUpload');
+const deleteFile = require('./deleteFile');
 module.exports = {
   authenticateUser,
   authenticateAdmin,
   getFileUploader,
-  getMultipleFilesUploader
+  getMultipleFilesUploader,
+  deleteFile
 };
